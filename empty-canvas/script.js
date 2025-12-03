9a244072a34b03847720a7516406f0c4dd627370
Norbert Schnell <norbert@schnell.space>
2025-12-03 23:42:23 +0100

    Initial commit

---
+++ empty-canvas/script.js
@@ -0,0 +1,14 @@
+const titleContainer = document.getElementById('title-container');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+window.addEventListener('resize', updateCanvasSize);
+updateCanvasSize();
+
+/*************************************************************
+ * canvas
+ */
+function updateCanvasSize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
